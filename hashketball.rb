19f45ca99c4c85ad36d5178b7103f403e3ecd14b
Eli Landau <eli.b.landau@gmail.com>
2020-06-10 22:55:06 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,7 +6,8 @@ game = {
     :team_name => "Brooklyn Nets",
     :colors => "Black, White",
     :players => [
-      "Alan Anderson" => {
+      {
+      "Alan Anderson" => 
         :number => 0,
         :shoe => 16,
         :points => 22,

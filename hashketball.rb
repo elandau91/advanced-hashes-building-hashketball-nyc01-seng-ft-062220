b9d3658dde588b82d514d817a36993f546ea5080
Eli Landau <eli.b.landau@gmail.com>
2020-06-10 22:33:06 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -8,8 +8,18 @@ game = {
     :players => [
       "Alan Anderson" => {
         :number => 0,
-        
-      }
+        :shoe => 16,
+        :points => 22,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 1
+      },
+      
+      
+      
+      
       ]
     
     
